16b35f7d6af13f49ca0bf005cd75e137b0beb31b
Jesus Manuel <fammily152@gmail.com>
2022-03-15 08:53:29 -0400

    Agregar Proyecto

---
+++ Js/agregarPalabra.js
@@ -0,0 +1,186 @@
+//BOTÓN E INPUT
+var botonAgregarPalabra = document.querySelector("#boton-agregar-palabra");
+var ingresarNuevaPalabra = document.querySelector("#ingresar-nueva-palabra");
+var textoBoton = document.querySelector("#texto-boton");
+
+var click = -1;
+var entrada = "";
+var palabrasInvalidas = [];
+var palabrasValidas = [];
+var audio1 = audio = document.getElementById("audio1"); //audio error entrada
+var audio2 = audio = document.getElementById("audio2"); //audio boton
+
+//Evento para agregar palabra
+botonAgregarPalabra.addEventListener("click", function (event) {
+    event.preventDefault();
+    click *= (-1);
+    if (click > 0) {
+        entrada = "";
+        activarAnimacion();
+        audio2.load();
+        audio2.play();
+    } else {
+        entradas = captureInput();
+        if (!validarEntrada(entradas)) {
+            agregarPalabra(palabrasValidas, listaDepalabras);
+            ingresarNuevaPalabra.value = "";
+            desactivarAnimacion();
+            audio2.load();
+            audio2.play();
+        } else {
+            click = 1;
+            errorEntrada();
+            audio1.load();
+            audio1.play();
+        }
+    }
+});
+
+//Función para capturar la entrada
+function captureInput() {
+    return (document.querySelector("#ingresar-nueva-palabra").value.toUpperCase());
+}
+
+function validarEntrada(entradas) {
+    var palabraInvalida = false;
+    var entrada = entradas.split(" ");
+    if (entradas.length != 0) {
+        for (var i = 0; i < entrada.length; i++) {
+            if (entrada[i].length < 3 || entrada[i] > 17) {
+                palabraInvalida = true;
+                break;
+            } else {
+                for (var j = 0; j < entrada[i].length; j++) {
+                    if ((entrada[i].charCodeAt(j) < 65 || entrada[i].charCodeAt(j) > 90) && entrada[i].charCodeAt(j) != 209) {
+                        palabraInvalida = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return palabraInvalida;
+}
+
+//Función para verificar que la palabra ingresada no esté repetida
+function agregarPalabra(entrada, listaDepalabras) {
+    entrada.forEach(function (palabra) {
+        if (!contiene(palabra, listaDepalabras)) {
+            listaDepalabras.push(palabra);
+        }
+    });
+}
+
+//ANIMACIONES DEL BOTÓN
+function activarAnimacion() {
+    botonAgregarPalabra.classList.remove("desplazar-original");
+    botonAgregarPalabra.classList.add("desplazar-izquierda");
+    ingresarNuevaPalabra.style.backgroundColor = "white";
+    ingresarNuevaPalabra.classList.remove("desplazar-original");
+    ingresarNuevaPalabra.classList.add("desplazar-derecha");
+    textoBoton.classList.remove("visible");
+    textoBoton.classList.add("invisible");
+    setTimeout(function () {
+        textoBoton.innerHTML = "Almacenar palabra";
+        textoBoton.classList.add("visible");
+    }, 800);
+
+}
+
+function desactivarAnimacion() {
+    botonAgregarPalabra.classList.remove("desplazar-izquierda");
+    botonAgregarPalabra.classList.add("desplazar-original");
+    ingresarNuevaPalabra.classList.remove("desplazar-derecha");
+    ingresarNuevaPalabra.classList.add("desplazar-original");
+    if (entradas.length > 0) {
+        textoBoton.classList.remove("visible");
+        textoBoton.classList.add("invisible");
+
+        setTimeout(function () {
+            textoBoton.innerHTML = "¡Palabra guardada!";
+            textoBoton.classList.remove("invisible");
+            textoBoton.classList.add("visible");
+        }, 800);
+
+        setTimeout(function () {
+            textoBoton.classList.remove("visible");
+            textoBoton.classList.add("invisible");
+        }, 1600);
+
+        setTimeout(function () {
+            textoBoton.innerHTML = "Agregar palabra";
+            textoBoton.classList.remove("invisible");
+            textoBoton.classList.add("visible");
+        }, 2400);
+    } else {
+        textoBoton.classList.remove("visible");
+        textoBoton.classList.add("invisible");
+
+        setTimeout(function () {
+            textoBoton.innerHTML = "Agregar palabra";
+            textoBoton.classList.remove("invisible");
+            textoBoton.classList.add("visible");
+        }, 800);
+    }
+}
+
+function errorEntrada() {
+    botonAgregarPalabra.classList.remove("desplazar-izquierda");
+    textoBoton.classList.remove("visible");
+    textoBoton.classList.add("invisible");
+    botonAgregarPalabra.classList.add("erratico-derecha");
+
+    setTimeout(function () {
+        textoBoton.innerHTML = "Error. Palabra inválida";
+        textoBoton.classList.remove("invisible");
+        textoBoton.classList.add("visible");
+        botonAgregarPalabra.classList.remove("erratico-derecha");
+        botonAgregarPalabra.classList.add("erratico-izquierda");
+    }, 100);
+
+    setTimeout(function () {
+        botonAgregarPalabra.classList.remove("erratico-izquierda");
+        botonAgregarPalabra.classList.add("erratico-derecha");
+    }, 200);
+
+    setTimeout(function () {
+        botonAgregarPalabra.classList.remove("erratico-derecha");
+        botonAgregarPalabra.classList.add("erratico-izquierda");
+    }, 300);
+
+    setTimeout(function () {
+        botonAgregarPalabra.classList.remove("erratico-izquierda");
+        botonAgregarPalabra.classList.add("erratico-derecha");
+    }, 400);
+
+    setTimeout(function () {
+        botonAgregarPalabra.classList.remove("erratico-derecha");
+        botonAgregarPalabra.classList.add("erratico-izquierda");
+    }, 500);
+
+    setTimeout(function () {
+        botonAgregarPalabra.classList.remove("erratico-izquierda");
+        botonAgregarPalabra.classList.add("erratico-derecha");
+    }, 600);
+
+    setTimeout(function () {
+        botonAgregarPalabra.classList.remove("erratico-derecha");
+        botonAgregarPalabra.classList.add("erratico-izquierda");
+    }, 700);
+
+    setTimeout(function () {
+        textoBoton.classList.remove("visible");
+        textoBoton.classList.add("invisible");
+    }, 1200);
+
+    setTimeout(function () {
+        textoBoton.innerHTML = "Almacenar palabra";
+        textoBoton.classList.remove("invisible");
+        textoBoton.classList.add("visible");
+        botonAgregarPalabra.classList.remove("erratico-izquierda");
+        botonAgregarPalabra.classList.add("desplazar-izquierda");
+    }, 1600);
+}
+
+
+//Span que muestre las palabras que no cumplen con los requisitos
---
+++ Js/ahorcado.js
@@ -0,0 +1,118 @@
+var iniciarJuego = false;
+var errores = 0;
+var palabra, letrasPalabra, letrasIngresadas, letrasCorrectas, letrasIncorrectas, tecla, lineas;
+
+//BOTÓN INICIAR JUEGO
+var botonIniciarJuego = document.querySelector("#boton-iniciar-juego");
+var inputInvisible = document.querySelector("#input-teclado");
+var screen = document.querySelector("canvas");
+
+screen.addEventListener("click", function (event) {
+    if (iniciarJuego) {
+        event.preventDefault();
+        inputInvisible.focus();
+    }
+});
+
+//Método para ingresar teclas apto para móviles con teclado suave
+
+//Método para ingresar teclas ideal para escritorio
+
+//Evento para (re)iniciar el juego al escuchar el click del mouse
+botonIniciarJuego.addEventListener("click", function (event) {
+    event.preventDefault();
+    audio2.load();
+    audio2.play();
+    inputInvisible.focus();
+    apagar(); //apagar animaciones (si las hay)
+    iniciarJuego = true;
+    errores = 0;
+    palabra = palabraAleatoria();
+    iniciarDibujo(palabra);
+    letrasPalabra = letrasSinRepetir(palabra);
+    letrasIngresadas = [];
+    letrasCorrectas = [];
+    letrasIncorrectas = [];
+    lineas = calcularLineas();
+    escribirLetrasCorrectas(lineas);
+});
+
+//Función que activa el juego
+inputInvisible.addEventListener("input", function () {
+    tecla = inputInvisible.value.toUpperCase();
+    inputInvisible.value = "";
+    if (iniciarJuego) { //sólo se evalúa si el juego está iniciado y no se está agregando una palabra
+        if (teclaValida(tecla)) { //valida el tipo de tecla ingresada
+            if (!contiene(tecla, letrasIngresadas)) { //evalua si ya se ingreso la tecla
+                letrasIngresadas.push(tecla);
+                letrasIngresadas.sort();
+                if (contiene(tecla, letrasPalabra)) { //evalua si la palabra contiene la tecla
+                    letrasCorrectas.push(tecla);
+                    letrasCorrectas.sort();
+                    lineas = transcribirLetra(lineas, tecla);
+                    limpiarPantalla(0, alto * 0.75, ancho, alto);
+                    escribirLetrasCorrectas(lineas); //La grafica en su lugar
+                } else {
+                    errores++;
+                    dibujarErrores(errores);
+                    letrasIncorrectas.push(tecla);
+                    limpiarPantalla(0, alto * 0.62, ancho, alto * 0.1);
+                    escribirLetraIncorrectas(letrasIncorrectas); //La grafica con las letras incorrectas
+                }
+                if (ganar()) { //Evalua si se ganó el juego
+                    iniciarJuego = false;
+                    escribir("¡GANASTE!", true);
+                    encender();
+                    hombrecitoSalvado();
+                }
+                if (perder()) {
+                    iniciarJuego = false;
+                    escribir("GAME OVER", true);
+                    dibujarCarita(0.6015, 0.236, false);
+                    palabraCorrecta();
+                }
+            }
+        }
+    }
+});
+
+//Función para seleccionar palabra aleatoria del banco de palabras
+function palabraAleatoria() {
+    var i = Math.round(Math.random() * ((listaDepalabras).length - 1));
+    return listaDepalabras[i].toUpperCase();
+}
+
+//Función que retorna un array con las letras que contiene la palabra aleatoria (ordenadas y sin repetir)
+function letrasSinRepetir(string) {
+    var letras = [];
+    var array = string.split('');
+    for (var i = 0; i < array.length; i++) {
+        if (!contiene(array[i], letras)) {
+            letras.push(array[i]);
+        }
+    }
+    return letras.sort();
+}
+
+/*Validación de la tecla por tamaño (deja fuera teclas como SHIFT, ALT, ENTER, etc)
+y utilizando código ASCII (deja fuera caracteres especiales a excepción de la ñ)*/
+function teclaValida(tecla) {
+    return (tecla.length == 1 && ((tecla.charCodeAt() >= 65 && tecla.charCodeAt() <= 90) || tecla.charCodeAt() == 209));
+}
+
+//Función para evaluar si un array contiene un letra determinada
+function contiene(elemento, lista) {
+    return lista.includes(elemento);
+}
+
+//Función para evaluar si el usuario acertó la palabra
+function ganar() {
+    return (letrasCorrectas.length == letrasPalabra.length);
+}
+
+//Función para evaluar si el usuario perdió la partida
+function perder() {
+    return (errores == 9);
+}
+
+
